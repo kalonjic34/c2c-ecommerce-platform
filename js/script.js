c017ad9578459b354736ad8be8d44229fd420c68
Christian <kalonjic34@gmail.com>
2026-06-02 18:23:43 +0200

    Fix button class name and implement add-to-cart functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+$(".btn-add").click(function () {
+    let itemId = $(this).data("id")
+    $.post("cart-php", { id: productId }, function (response) {
+        $("#items-count").text(response.court);
+    })
+})
